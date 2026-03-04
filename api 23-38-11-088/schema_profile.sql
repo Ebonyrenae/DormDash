aaf8e10427f1a5d7fac36e214ea819c6c3b265bd
neerajArde <narde@buffalo.edu>
2026-03-04 14:39:29 -0500

    Overwrite remote branch with this commit

--- api 23-38-11-088/schema_profile.sql
+++
@@ -1,5 +0,0 @@
--- Run on your users table. If columns already exist, ignore "Duplicate column" errors.
-ALTER TABLE users ADD COLUMN university VARCHAR(255) DEFAULT '';
-ALTER TABLE users ADD COLUMN program VARCHAR(255) DEFAULT '';
-ALTER TABLE users ADD COLUMN bio TEXT;
-ALTER TABLE users ADD COLUMN experience TEXT;
